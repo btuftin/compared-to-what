e41546163d51bdd2a0703f4ecf025b7bfdb50513
Bjoernar Tuftin <btuftin@gmail.com>
2020-07-14 18:18:29 -0400

    Server activities updated.

--- app.R
+++ app.R
@@ -29,11 +29,12 @@ label_texts <- read_csv(file_labels)
 # Various aspects of the plot are defined in this theme
 plot_theme <- theme(aspect.ratio = 1) + theme_bw()
 
-# Some reused arguments
+# Some reused arguments. OBS! The values have to be the same as the column
+# headers in comparisons and user_data
 effect_size_choices = c(`Cohen's D`= "cohens",
                         `Hazard ratio` = "hr",
                         `Odds ratio` = "or",
-                        `Risk rifference` = "rd",
+                        `Risk difference` = "rd",
                         `Risk ratio` = "rr")
 
 prevalence_choices = c(`Prevalence of Outcome` = "prev_outc",
@@ -44,6 +45,17 @@ study_design_choices = comparisons$design
 
 # Fetch fields of study from the tibble and add "Custom" for custom reference
 study_field_choices = c(comparisons$field, "Custom")
+
+# User entered effect sizes need to be converted to all other effect size types
+# allowed for plotting
+effect_size_convert <- function(type_out, type_in, value){
+    if (type_out == type_in)
+        return (value)
+    else
+        #!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#!#
+        # Currently there is no conversion! Only Zuul!
+        return (value * runif(1))
+}
 
 # The top level in the user interface is a navbarPage. It creates a navigation
 # bar on the top of the page with a tab for each of the tabPanels included
@@ -74,7 +86,7 @@ ui <- navbarPage("Compared to What?",
                   fluidRow(column(width = 2,
                           numericInput("effect_size_input", "Value", 0, min = 0, max = 10)),
                        column(width = 2,
-                          selectInput("effect_size_type",
+                          selectInput("effect_size_type_input",
                                       "Type",
                                       choices = effect_size_choices)),
                        column(width = 2,
@@ -104,7 +116,7 @@ ui <- navbarPage("Compared to What?",
                    sidebarPanel(
                        HTML("<p><b>Effect size</b></p>"),
                        p("Plot effect size as:"),
-                       selectInput("effect_size_type",
+                       selectInput("effect_size_type_to_plot",
                                    NULL,
                                    choices = effect_size_choices),
                        HTML("<p><b>Prevalence</b></p>"),
@@ -185,28 +197,131 @@ ui <- navbarPage("Compared to What?",
 # Define server logic required to draw a histogram
 server <- function(input, output) {
 
+    all_references <- reactiveValues(data = comparisons)
+    
+    references <- reactive({
+        designs <- if(!is.null(input$designs_chosen)) input$designs_chosen 
+                    else study_design_choices
+        fields <- if(!is.null(input$fields_chosen)) input$fields_chosen
+                    else study_field_choices
+        all_references$data %>% filter(design %in% designs & field %in% fields) %>%
+            mutate(effect_size = pull(., input$effect_size_type_to_plot)) %>%
+            mutate(prevalence = pull(., input$prevalence_type))
+    })
+    
     users_data <- reactiveValues(data = NULL)
     
+    
+    # When the users clicks the apply button the Index Association should be
+    # updated, if the values aren't the default 0s
+    # Currently it doesn't call the effect_size_convert for recording the upper
+    # and lower bound, which will be required.
     observeEvent(
         input$apply_button,
         {
+            users_data$data <- NULL
             if(!(input$effect_size_input == 0)&
-                !(input$prevalence_input == 0)&
-                !(input$precision== 0)) {
-               users_data$data <- tibble(rr = input$effect_size_input,
-                      prev_outc = input$prevalence_input,
-                      error_max = input$effect_size_input + input$precision,
-                      error_min = input$effect_size_input - input$precision)
-                } else
-                {users_data$data <- NULL}
-    })
+                                !(input$prev_exp_input == 0)&
+                                !(input$prev_outc_input == 0)&
+                                !(input$upper_bound <= input$lower_bound))
+            {
+               temp <- tibble(prev_exp = input$prev_exp_input,
+                                         prev_outc = input$prev_outc_input,
+                                         error_max = input$upper_bound,
+                                         error_min = input$lower_bound)
+               temp <- temp %>% add_column(cohens = effect_size_convert("cohens",
+                       input$effect_size_type_input, input$effect_size_input))
+               temp <- temp %>% add_column(hr = effect_size_convert("hr",
+                       input$effect_size_type_input, input$effect_size_input))
+               temp <- temp %>% add_column(or = effect_size_convert("or",
+                       input$effect_size_type_input, input$effect_size_input))
+               temp <- temp %>% add_column(rd = effect_size_convert("rd",
+                       input$effect_size_type_input, input$effect_size_input))
+               temp <- temp %>% add_column(rr = effect_size_convert("rr",
+                       input$effect_size_type_input, input$effect_size_input))
+               users_data$data <- temp
+               temp <- NULL
+               removeNotification(id = "index_error")
+            } else {
+                showNotification("Invalid or missing data",
+                                 duration = NULL,
+                                 id = "index_error", 
+                                 type = "error")
+            }
+        }
+    )
+
+    # When the users clicks the apply button on the table tab a custom reference
+    # should be added. This mimics a lot of the code for the index association
+    observeEvent(
+        input$custom_apply,
+        {
+            if(!(is.null(input$ref_label))&
+               !(input$prev_exp_custom == 0)&
+               !(input$prev_outc_custom == 0)&
+               !(is.null(input$study_design)))
+            {
+                all_references$data <- all_references$data %>%
+                    add_row(
+                        label = input$ref_label,
+                        rr = effect_size_convert("rr",
+                                                 input$effect_size_t_custom,
+                                                 input$effect_size_in_custom),
+                        hr = effect_size_convert("hr",
+                                                 input$effect_size_t_custom,
+                                                 input$effect_size_in_custom),
+                        or = effect_size_convert("or",
+                                                 input$effect_size_t_custom,
+                                                 input$effect_size_in_custom),
+                        cohens = effect_size_convert("cohens",
+                                                     input$effect_size_t_custom,
+                                                     input$effect_size_in_custom),
+                        rd = effect_size_convert("rd",
+                                                 input$effect_size_t_custom,
+                                                 input$effect_size_in_custom),
+                        orig. = input$effect_size_t_custom,
+                        prev_outc = input$prev_outc_custom,
+                        prev_exp = input$prev_exp_custom,
+                        field = "Custom",
+                        design = input$study_design,
+                        ref = "User supplied")
+                removeNotification(id = "custom_error")
+            } else {
+                showNotification("Invalid or missing data",
+                                 duration = NULL,
+                                 id = "custom_error", 
+                                 type = "error")
+            }
+        }
+    )
     
+    # When the users clicks the apply button on the table tab a custom reference
+    # should be added. This mimics a lot of the code for the index association
+    observeEvent(
+        input$custom_delete,
+        {
+            all_references$data <- all_references$data %>%
+                    filter(label != input$ref_label)
+        }
+    )
+        
+    # The current index association to plot depends on what effect
+    # size has been chosen on the plot type
+    # This needs to be updated to include the upper and lower bound
+    # since they too wil change with the effect size type.
+    # That will be a whole other mess.
+    index_association <- reactive({
+        users_data$data %>% mutate(effect_size = 
+                                       pull(., input$effect_size_type_to_plot)) %>%
+            mutate(prevalence = pull(., input$prevalence_type))
+    })
+        
     output$plot <- renderPlot({
         p <- ggplot() +
-            geom_point(data = comparisons, aes(prev_outc, rr))
+            geom_point(data = references(), aes(prevalence, effect_size))
         if (!is.null(users_data$data)) {
-            p <- p + geom_point(data = users_data$data, aes(prev_outc, rr), shape = 1, fill = "white", size = 5)
-            p <- p + geom_errorbar(data = users_data$data, aes(x = prev_outc, ymax = error_max, ymin = error_min))
+            p <- p + geom_point(data = index_association(), aes(prevalence, effect_size), shape = 1, fill = "white", size = 5)
+            p <- p + geom_errorbar(data = index_association(), aes(x = prevalence, ymax = error_max, ymin = error_min))
          }
         p <- p + plot_theme
         
@@ -214,15 +329,16 @@ server <- function(input, output) {
     })
 
     # Generate an HTML table view of the data ----
-    output$table <- renderDT(datatable(comparisons,
+    # Commented out to examine the derived table for debugging
+    output$table <- renderDT(datatable(references()[1:12],
                                     options = list(autoWidth = TRUE,
                                                    scrollX = TRUE,
                                                    columnDefs = list(
                                                        list(targets = 0,
                                                             visible = FALSE),
-                                                       list(targets = 1, 
+                                                       list(targets = 1,
                                                             width = '200px'),
-                                                       list( targets = 12, 
+                                                       list( targets = 12,
                                                              width = '300px'))
                                                    )
                                     )
